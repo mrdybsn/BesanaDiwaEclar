7eb4e6d6bf273b857f6360811c6c80efbee26981
mrdybsn <mardybesana0705@gmail.com>
2026-03-28 14:50:24 +0800

    Header and Sidebar

---
+++ client/src/layout/AppHeader.tsx
@@ -0,0 +1,108 @@
+import { Link } from "react-router-dom";
+import { useHeader } from "../contexts/HeaderContext";
+import { useSidebar } from "../contexts/SidebarContext";
+
+const AppHeader = () => {
+  const {isOpen, toggleUserMenu} = useHeader();
+  const {toggleSidebar} = useSidebar();
+
+  return (
+    <>
+    {isOpen && (
+      <div className="fixed inset-0 z-40" onClick={toggleUserMenu}></div>
+    )}
+    <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+      <div className="px-3 py-3 lg:px-5 lg:pl-3">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center justify-start rtl:justify-end">
+            <button 
+              data-drawer-target="logo-sidebar" 
+              data-drawer-toggle="logo-sidebar" 
+              aria-controls="logo-sidebar" 
+              type="button" 
+              onClick={toggleSidebar}
+              className="infline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden    hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              >
+                <span className="sr-only">Open sidebar</span>
+                <svg 
+                  className="w-6 h-6" 
+                  aria-hidden="true" 
+                  fill="currentColor" 
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg" 
+                >
+                  <path 
+                    stroke="currentColor" 
+                    strokeLinecap="round" 
+                    strokeWidth="2" 
+                    d="M5 7h14M5 12h14M5 17h10"
+                  ></path>
+                </svg>
+            </button>
+            <a href="https://flowbite.com" className="flex ms-2 md:me-24">
+              <img 
+                src="https://flowbite.com/docs/images/logo.svg" 
+                className="h-8 me-3" 
+                alt="FlowBite Logo" 
+              />
+              <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
+                Flowbite
+              </span>
+            </a>
+          </div>
+          <div className="flex items-center">
+              <div className="flex items-center ms-3">
+                <div>
+                  <button 
+                    type="button" 
+                    onClick={toggleUserMenu}
+                    className="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" 
+                    aria-expanded="false" 
+                    data-dropdown-toggle="dropdown-user"
+                  >
+                    <span className="sr-only">Open user menu</span>
+                    <img 
+                      className="w-8 h-8 rounded-full" 
+                      src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" 
+                      alt="user photo"/>
+                  </button>
+                </div>
+                <div 
+                  className={`absolute right-8 top-9 min-w-50 z-50 ${
+                    isOpen ? 'block' : 'hidden'
+                  } my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-sm dark:bg-gray-700 dark:divide-gray-600`} 
+                  id="dropdown-user"
+                  >
+                  <div className="px-4 py-3" role="none">
+                    <p 
+                      className="text-sm text-gray-900 dark:text-white" 
+                      role="none"
+                    >
+                      Neil Sims
+                    </p>
+                    <p 
+                      className="text-sm font-medium text-gray-900 truncate dark:text-gray-300" 
+                      role="none"
+                    >
+                      neil.sims@flowbite.com
+                    </p>
+                  </div>
+                  <ul className="p-2 text-sm text-body font-medium" role="none">
+                    <li>
+                      <Link
+                        to="#" 
+                        className="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded" role="menuitem">Sign out
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+    </nav>
+    </>
+  )
+}
+
+export default AppHeader
